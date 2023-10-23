f3b2c2add529f7969d10af170121e2d4856f38df
yws1502 <yws1502@naver.com>
2023-10-23 22:02:34 +0900

    setting: web socket 설정

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
 import express from "express";
 
 const app = express();
@@ -5,15 +8,19 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "pug");
-
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
-
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:${port}`);
 
-app.listen(port, handleListen);
+// http server
+const server = http.createServer(app);
+
+// ws server
+const wss = new WebSocket.Server({ server });
+
+server.listen(port, handleListen);
