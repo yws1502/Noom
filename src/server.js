304d84f2f95ff35d1b9378ae481c09cb73c15d5d
yws1502 <yws1502@naver.com>
2023-10-23 21:35:58 +0900

    🎉 - frontend 초기 개발 환경 설정

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const app = express();
 
 const port = 3000;
 
+app.set("view engine", "pug");
+
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:${port}`);
 
 app.listen(port, handleListen);
