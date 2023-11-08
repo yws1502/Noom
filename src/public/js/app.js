f0440ba55d8aa9ea18d673c953842639df4d1299
yws1502 <yws1502@naver.com>
2023-11-08 19:38:55 +0900

    style: 비디오 관련 마크업, 스타일링 적용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
 const socket = io();
+
+const myFace = document.querySelector("#myFace");
+
+const muteBtn = document.querySelector("#mute");
+
+const cameraBtn = document.querySelector("#camera");
+
+let myStream;
+
+let muted = false;
+
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+
+    myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  muted = !muted;
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+  } else {
+    muteBtn.innerText = "Mute";
+  }
+}
+
+function handleCameraClick() {
+  cameraOff = !cameraOff;
+  if (!cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+
+cameraBtn.addEventListener("click", handleCameraClick);
