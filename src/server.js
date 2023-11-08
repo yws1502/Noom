836e829c20cb8e1b864193725122a8d2d4519f69
yws1502 <yws1502@naver.com>
2023-11-08 19:26:50 +0900

    fix: webRTC를 위한 코드 제거

--- src/server.js
+++ src/server.js
@@ -17,8 +17,6 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:${port}`);
-
 // http server
 const httpServer = http.createServer(app);
 
@@ -30,105 +28,15 @@ const wsServer = new Server(httpServer, {
   },
 });
 
-instrument(wsServer, {
-  auth: false,
-});
-
 /*
 socket io admin ui
 
 link: https://admin.socket.io/#/
 */
-
-function publicRooms() {
-  const { sockets } = wsServer;
-
-  const { sids, rooms } = sockets.adapter;
-
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) publicRooms.push(key);
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-
-  // event is like enter_room, anymore
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-
-    done();
-
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-
-  // disconnecting event는 socket이 방을 떠나기 바로 직전에 발생 함.
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
-
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, roomName, done) => {
-    socket.to(roomName).emit("new_message", `${socket.nickname}: ${msg}`);
-
-    done();
-  });
-
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+instrument(wsServer, {
+  auth: false,
 });
 
-/*
-import WebSocket from "ws";
-
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-  console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  console.log("Connected to Browser ✅");
-
-  socket["nickname"] = "Anon";
-
-  socket.on("close", onSocketClose);
-
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
-  });
-});
-*/
+const handleListen = () => console.log(`Listening on http://localhost:${port}`);
 
 httpServer.listen(port, handleListen);
