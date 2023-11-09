fe9c015ca96dc5cebb24694697df2763c86f5b84
yws1502 <yws1502@naver.com>
2023-11-09 20:04:58 +0900

    feat: sender를 통해 변경된 track 반영하는 로직 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -100,6 +100,16 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
